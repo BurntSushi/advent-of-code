93fc69250fb14ce30fee1bdc39daa4109797dfd2
Andrew Gallant <jamslam@gmail.com>
2018-12-01 23:33:38 -0500

    rearrange structure to use one Cargo project per day

--- 2018/aoc01/src/main.rs
+++ 2018/aoc01/src/main.rs
@@ -4,12 +4,28 @@ use std::io::{self, Read, Write};
 type Result<T> = ::std::result::Result<T, Box<::std::error::Error>>;
 
 fn main() -> Result<()> {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input)?;
+
+    part1(&input)?;
+    part2(&input)?;
+    Ok(())
+}
+
+fn part1(input: &str) -> Result<()> {
+    let mut freq = 0;
+    for line in input.lines() {
+        let change: i32 = line.parse()?;
+        freq += change;
+    }
+    writeln!(io::stdout(), "{}", freq)?;
+    Ok(())
+}
+
+fn part2(input: &str) -> Result<()> {
     let mut freq = 0;
     let mut seen = HashSet::new();
     seen.insert(0);
-
-    let mut input = String::new();
-    io::stdin().read_to_string(&mut input)?;
 
     loop {
         for line in input.lines() {
