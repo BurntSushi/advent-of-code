75f8cc5442965f5e5f664c328b882f09fd8ffa92
তানজীম হোসেন <audacioustux@protonmail.com>
2019-11-29 18:21:07 +0600

    Update main.rs
    warning: trait objects without an explicit `dyn` are deprecated

--- aoc01/src/main.rs
+++ aoc01/src/main.rs
@@ -1,7 +1,7 @@
 use std::collections::HashSet;
 use std::io::{self, Read, Write};
 
-type Result<T> = ::std::result::Result<T, Box<::std::error::Error>>;
+type Result<T> = ::std::result::Result<T, Box<dyn ::std::error::Error>>;
 
 fn main() -> Result<()> {
     let mut input = String::new();
